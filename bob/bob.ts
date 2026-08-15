bd62b35919f34d6d35674d7f791ce940ce743e2d
Marcos Cerdán <marcos.cerdan.amat@gmail.com>
2026-08-16 00:49:00 +0200

    bob done

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,3 +1,26 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+export function hey(message: string): string {
+
+  const endsWithQuestionMark = message.trim().endsWith('?');
+  const allCapitals = message === message.toUpperCase() && /[A-Z]/.test(message);
+  const emptySentence = message.replace(/\s/g, '').length == 0;
+
+  if (emptySentence) {
+    return "Fine. Be that way!";
+  }
+
+  if (allCapitals && endsWithQuestionMark) {
+    return "Calm down, I know what I'm doing!";
+  }
+
+  if (allCapitals) {
+    return "Whoa, chill out!";
+  }
+
+  if (endsWithQuestionMark) {
+    return "Sure.";
+  }
+
+  return "Whatever.";
+
+
 }
